95ba77381932973a723aab4e4ef4d8c29286b2c9
Matthew Ogtong <ogtongm@gmail.com>
2020-07-30 23:07:28 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -21,10 +21,12 @@ function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-  sayMy();
+
 
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
+
   }
+  sayMy();
 }
